1eb25da89be4b3e8440929125719041adcc3635d
iyade2336 <84434604+iyade2336@users.noreply.github.com>
2024-02-24 05:03:52 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:8000/predict/'
-files = {'file': open('C:/Users/iyade/OneDrive/Desktop/big_project-main/big_project-main/class 5.JPG', 'rb')}  # تأكد من تحديث المسار إلى مسار الصورة التي تريد تصنيفها
-
-response = requests.post(url, files=files)
-print(response.json())
